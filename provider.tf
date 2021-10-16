5c74afbe9228067f564e4121dd934e6105b9d68d
Mukesh-dev-ops <57295600+Mukesh-dev-ops@users.noreply.github.com>
2021-10-16 23:14:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
   access_key="AKIA52TKJW62MY2H7OWG"
-  secret_access_key="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"
+  secret_key="8Ji0lzPt/oGelAx19f71il2fpIDSmaEYP41q+Ng7"
   region="us-east-2"
 }
