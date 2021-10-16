c7d6ae874c86efaf93df5cae5f9f905456a9aa08
Mukesh-dev-ops <57295600+Mukesh-dev-ops@users.noreply.github.com>
2021-10-16 23:10:43 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,8 +1,3 @@
-provider "aws"{
-  access_key="AKIA52TKJW62MY2H7OWG"
-  secret_access_key="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"
-  region="us-east-2"
-}
 resource "aws_instance" "webserver"{
  ami="ami-074cce78125f09d61"
  instance_type="t2.micro"
