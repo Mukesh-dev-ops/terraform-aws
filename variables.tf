ca8d0386e2205bce0ce0e1691a36ae7f1d705740
Mukesh-dev-ops <57295600+Mukesh-dev-ops@users.noreply.github.com>
2021-10-22 05:02:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
 
+variable "access_key" {
+  default = "AKIA52TKJW62MY2H7OWG"
+}
+variable "secret_key" {
+  default = "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"
+}
+variable "region" {
+ default = "us-east-2"
+}
+variable "ami" {
+ default = "ami-074cce78125f09d61"
+}
+variable "instance_type" {
+ default = "t2.micro"
+}
